7d299a2938ed45b8e8f23e3aed6e2d68e16e22d0
Albicocca224 <albicocca224@gmail.com>
2025-02-16 20:34:43 +0500

    Lab4 done

---
+++ Lab4/shop/src/app/products.ts
@@ -0,0 +1,100 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+  img: string;
+  kaspi_link: string;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'iPhone XR',
+    price: 799,
+    description: 'A large phone with one of the best screens',
+    img: "https://resources.cdn-kaspi.kz/img/m/p/h98/hec/63756182716446.jpg?format=gallery-large",
+    kaspi_link : "https://kaspi.kz/shop/p/apple-iphone-xr-64gb-chernyi-1004249/?srsltid=AfmBOooZ7YOGSw43ewn6m1-dCSQtZhfWaA-PqXbdxF9pHwiM1aQynGpQ"
+  },
+  {
+    id: 2,
+    name: 'iPhone 13 mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras',
+    img: "https://resources.cdn-kaspi.kz/img/m/p/h29/h83/64200298430494.jpg?format=gallery-large",
+    kaspi_link : "https://kaspi.kz/shop/p/apple-iphone-13-mini-256gb-sinii-102298484/?c=750000000"
+  },
+  {
+    id: 3,
+    name: 'iPhone 12 Pro Max',
+    price: 299,
+    description: 'A great phone with one of the best cameras',
+    img: "https://resources.cdn-kaspi.kz/img/m/p/h3d/hbe/64083803635742.jpg?format=gallery-large",
+    kaspi_link : "https://kaspi.kz/shop/p/apple-iphone-12-pro-max-256gb-seryi-100658022/?c=750000000"
+  },
+  {
+    id: 4,
+    name: 'Samsung Galaxy S22',
+    price: 700,
+    description: 'FE 5G (256GB, 8GB) 6.5" AMOLED, Snapdragon 865, IP68 Water Resistant',
+    img: "https://resources.cdn-kaspi.kz/img/m/p/h85/h78/86009675743262.png?format=gallery-large",
+    kaspi_link : "https://kaspi.kz/shop/p/samsung-galaxy-s22-8-gb-256-gb-zelenyi-103667982/?c=750000000"
+  },
+  {
+    id: 5,
+    name: 'Samsung galaxy S23 ',
+    price: 999,
+    description: 'long battery life, bright display',
+    img: "https://resources.cdn-kaspi.kz/img/m/p/h5e/h53/69635680763934.jpg?format=gallery-large",
+    kaspi_link : "https://kaspi.kz/shop/p/samsung-galaxy-s23-ultra-12-gb-256-gb-chernyi-109174566/?c=750000000"
+  },
+  {
+    id: 6,
+    name: 'Xiaomi 11T',
+    price: 432,
+    description: '108mp tripple camera, NFC dual sim',
+    img: "https://resources.cdn-kaspi.kz/img/m/p/h45/hd1/64375879204894.jpg?format=gallery-large",
+    kaspi_link : "https://kaspi.kz/shop/p/xiaomi-11t-8-gb-128-gb-seryi-102860712/?c=750000000"
+  },
+  {
+    id: 7,
+    name: 'Xiaomi redmi 9A',
+    price: 299,
+    description: '2Gb + 32 gb',
+    img: "https://resources.cdn-kaspi.kz/img/m/p/h22/hbc/63754987274270.jpg?format=gallery-large",
+    kaspi_link : "https://kaspi.kz/shop/p/redmi-9a-2-gb-32-gb-sinii-100399568/?c=750000000"
+  },
+  {
+    id: 8,
+    name: 'Phone Standard',
+    price: 1000,
+    description: 'Apple iPhone 15 Pro Max',
+    img: "https://resources.cdn-kaspi.kz/img/m/p/ha3/hda/86319874048030.jpg?format=gallery-large",
+    kaspi_link : "https://kaspi.kz/shop/p/apple-iphone-15-pro-max-256gb-seryi-113138420/?c=750000000"
+  },
+  {
+    id: 9,
+    name: 'One plus nord 4',
+    price: 800,
+    description: '5G Enabled (5G service only available on T-Mobile and Google Fi)',
+    img: "https://resources.cdn-kaspi.kz/img/m/p/h9a/hcd/86777419989022.png?format=gallery-large",
+    kaspi_link : "https://kaspi.kz/shop/p/oneplus-nord-4-5g-12-gb-256-gb-chernyi-122347470/?c=750000000"
+  },
+  {
+    id: 10,
+    name: 'One plus nord N20',
+    price: 500,
+    description: '120Hz Fluid Display, Hasselblad Triple Camera, 65W Ultra Fast Charge',
+    img: "https://resources.cdn-kaspi.kz/img/m/p/hfc/ha5/65530200391710.jpg?format=gallery-large",
+    kaspi_link : "https://kaspi.kz/shop/p/oneplus-nord-n20-se-4-gb-64-gb-chernyi-107503551/?c=750000000"
+  },
+  
+
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
